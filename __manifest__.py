eb067a89990242cf720c5618faedcd7b9af7bc24
jekstartcoding <muhammadzakyaliyashfi@gmail.com>
2026-06-24 22:13:27 +0700

    feat: implement full RBAC and menu restrictions

--- __manifest__.py
+++ __manifest__.py
@@ -8,6 +8,7 @@
     "depends": ["base", "mail", "survey", "plugins_manajement_asesor", "plugins_lsp_survey"],
     "data": [
         "security/ir.model.access.csv",
+        "security/lsp_penilaian_rules.xml",
         "data/ir_sequence_data.xml",
         "views/hasil_asesmen_views.xml",
         "views/lsp_menus.xml",
